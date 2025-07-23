c70d4ee9d68c2a44e5e697c10e6a678642be6d7b
Lucas Garcia Marcos <lucas.marcos@careintelligence.ai>
2025-07-23 15:05:29 -0300

    Logica isometrica das terrinhas

--- main.py
+++ main.py
@@ -17,6 +17,27 @@ cursor_img = pygame.image.load("sprites/cursor.png").convert_alpha()
 cursor_img = pygame.transform.scale(cursor_img, (120, 120))
 cursor_img.set_colorkey((255, 255, 255))
 
+# Carregar o sprite do terreno
+terra_img = pygame.image.load("sprites/terra_seca.png").convert()
+terra_img = pygame.transform.scale(terra_img, (200, 200))
+terra_img.set_colorkey((255, 255, 255))
+
+def criar_terrinhas(screen, terra_img, linhas=3, colunas=3, pos_inicial_x=200, pos_inicial_y=200):
+    terrinhas = {}
+
+    for linha in range(linhas):
+        for coluna in range(colunas):
+            x = pos_inicial_x + (coluna * 80) + (linha * 80)
+            y = pos_inicial_y + (coluna * 35) - (linha * 35)
+            rect = terra_img.get_rect(center=(x, y))
+            terrinhas[(linha, coluna)] = {
+                "rect": rect,
+                "estado": "seco"
+            }
+            screen.blit(terra_img, rect)
+
+    return terrinhas
+
 running = True
 while running == True:
     for event in pygame.event.get():
@@ -25,6 +46,9 @@ while running == True:
 
     # Fundo
     screen.fill((0, 0, 0))
+
+    # desenhos
+    criar_terrinhas(screen, terra_img)
 
     # Posicao do mouse
     mouse_pos = pygame.mouse.get_pos()
