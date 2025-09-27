ccd2016242b20507388c7e29ef71a309601bfb09
Lucas Garcia Marcos <lucas.marcos@careintelligence.ai>
2025-09-27 18:56:43 -0300

    Fix: Reorder

---
+++ src/config.py
@@ -0,0 +1,36 @@
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+FPS = 60
+
+FUNDO_PATH = "sprites/fundo.png"
+
+# Caminhos básicos
+TERRA_PATH = "sprites/terra_seca.png"
+DEBUG = True
+
+FERRAMENTAS = {
+    0: {
+        "nome": "foice",
+        "menu_icon": "sprites/menu_foice.png",
+        "cursor": "sprites/cursor.png"
+    },
+    1: {
+        "nome": "agua", 
+        "menu_icon": "sprites/menu.png",
+        "cursor": "sprites/cursor.png"
+    },
+    2: {
+        "nome": "sementes",
+        "menu_icon": "sprites/menu.png",
+        "cursor": "sprites/cursor.png"
+    },
+    3: {
+        "nome": "fertilizante",
+        "menu_icon": "sprites/menu.png",
+        "cursor": "sprites/cursor.png"
+    },
+    4: {
+        "nome": "loja",
+        "menu_icon": "sprites/menu.png"
+    }
+}
